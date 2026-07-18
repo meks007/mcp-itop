b862527032ca9ad77b7fcd6ccd957da121866fc7
meks <23148641+meks007@users.noreply.github.com>
2026-07-18 13:27:14 +0200

    Delete helpers.py

--- helpers.py
+++
@@ -1,5 +0,0 @@
-# helpers.py
-# Superseded by the helpers/ package.
-# This file redirects all imports to the package and will be removed
-# once helpers/ is confirmed stable in production.
-from helpers import *  # noqa: F401, F403
