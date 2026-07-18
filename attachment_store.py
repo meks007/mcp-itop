eee7e5afa8e71c145a3fd9a3a2a022d09dfbd120
meks <23148641+meks007@users.noreply.github.com>
2026-07-18 13:27:05 +0200

    Delete attachment_store.py

--- attachment_store.py
+++
@@ -1,5 +0,0 @@
-# attachment_store.py
-# Superseded by the attachment_store/ package.
-# This file redirects all imports to the package and will be removed
-# once attachment_store/ is confirmed stable in production.
-from attachment_store import *  # noqa: F401, F403
